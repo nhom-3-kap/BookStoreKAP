147977132d3435cbcaf6e47c663c644576d76342
kiet pham <kietpyyy@gmail.com>
2024-06-01 00:49:53 +0700

    today I do New Releases Page#1

---
+++ BookStoreKAP/Controllers/NewReleasesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreKAP.Controllers
+{
+    public class NewReleasesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

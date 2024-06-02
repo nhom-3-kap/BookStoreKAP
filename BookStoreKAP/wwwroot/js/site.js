f1faf5ccf0616f1ec3f3ffc6b8836bc7ac441163
kiet pham <kietpyyy@gmail.com>
2024-06-03 00:47:44 +0700

    update home and new release

--- BookStoreKAP/wwwroot/js/site.js
+++ BookStoreKAP/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    $(".card-heading[data-url]").each(function (index, item) {
+        $(item).on("click", function () {
+            const url = $(this).attr("data-url")
+            location.href = url
+        })
+    })
+})

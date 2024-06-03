69fa1e86c05d7834d7aad2174fb1b5cb92b15016
TaiLe <taic21a.th1@gmail.com>
2024-06-04 01:36:28 +0700

    update: create carousel home page

--- BookStoreKAP/wwwroot/js/home.js
+++ BookStoreKAP/wwwroot/js/home.js
@@ -1,9 +1,9 @@
-﻿$(document).ready(function () {
-    $(".owl-carousel#carousel-new-release").owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        responsive: {
+﻿function initializeOwlCarousel(selector, options) {
+    $(selector).owlCarousel({
+        loop: options.loop || true,
+        margin: options.margin || 10,
+        nav: options.nav || true,
+        responsive: options.responsive || {
             0: {
                 items: 1
             },
@@ -14,5 +14,53 @@
                 items: 5
             }
         }
+    });
+
+    if (options.controlLeft) {
+        $(`${selector} ~ .carousel-controls ${options.controlLeft}`).on("click", function () {
+            $(selector).trigger('prev.owl.carousel');
+        });
+    }
+
+    if (options.controlRight) {
+        $(`${selector} ~ .carousel-controls ${options.controlRight}`).on("click", function () {
+            $(selector).trigger('next.owl.carousel');
+        });
+    }
+}
+
+$(document).ready(function () {
+    initializeOwlCarousel("#carousel-new-release", {
+        controlLeft: '.control-left',
+        controlRight: '.control-right'
+    });
+
+    initializeOwlCarousel("#carousel-bestsellers", {
+        controlLeft: '.control-left',
+        controlRight: '.control-right'
+    });
+
+    initializeOwlCarousel("#carousel-feature-book", {
+        controlLeft: '.control-left',
+        controlRight: '.control-right'
+    });
+
+    initializeOwlCarousel("#slider-content", {
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    initializeOwlCarousel("#carousel-sold-book", {
+        controlLeft: '.control-left',
+        controlRight: '.control-right'
     });
 });

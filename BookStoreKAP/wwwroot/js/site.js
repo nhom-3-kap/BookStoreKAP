d5ff2f6a4f0e76c16da8c76f0fac4d796b713791
kiet pham <kietpyyy@gmail.com>
2024-06-06 06:59:04 +0700

    update HomePage+update NewRelease+ Add Page BookRequest

--- BookStoreKAP/wwwroot/js/site.js
+++ BookStoreKAP/wwwroot/js/site.js
@@ -6,3 +6,27 @@
         })
     })
 })
+
+    
+    src = "https://code.jquery.com/jquery-3.6.0.min.js" 
+$(document).ready(function () {
+     var rotation = 0;
+    $(".side-bar-item").each(function (index,item) {
+
+        $(item).on("click", function () {
+            if ($(this).find(".wrap-icon").hasClass("rotate")) {
+                $(this).find(".wrap-icon").removeClass("rotate")
+                $(this).css("height", "38px")
+
+            } else {
+                $(this).find(".wrap-icon").addClass("rotate")
+                $(this).css("height", `${$(this).find(".side-bar-item-child").height() + 38}px`)
+                console.log(`${$(this).find(".side-bar-item-child").height() + 10}px`)
+
+
+            }
+        });
+
+    })
+});
+                 

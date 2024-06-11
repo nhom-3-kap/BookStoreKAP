49d25a40ff31c27e9326c2015ec309d111d3f610
TaiLe <taic21a.th1@gmail.com>
2024-06-12 02:40:09 +0700

    side bar admin

---
+++ BookStoreKAP/Areas/Admin/Controllers/CategoriesManagerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreKAP.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookStoreKAP/wwwroot/js/admin.js
@@ -0,0 +1,31 @@
+﻿const handleGetActiveMenuSideBar = () => {
+    var [key, value] = $(location).attr('href').split('?')[1].split('=');
+
+    ["CM", "P", "USER", "ROLE", "ODER", "INFO"].forEach((menuKey) => {
+        let menuKeyOperator = menuKey;
+        if (key === "menuKey" && value.endsWith("#")) {
+            menuKeyOperator = `${menuKey}#`;
+        }
+
+        if (menuKeyOperator === value) {
+            const element = $(`.admin-side-bar-menu-item-control[data-menu-key='${menuKey}']`);
+            $(element).addClass("active");
+            $(element).next(".admin-side-bar-menu-item-child").addClass("active");
+        }
+    });
+};
+
+$(document).ready(function () {
+    const handleToggleMultipleMenu = () => {
+        $(".admin-side-bar-menu-item-control").on("click", function () {
+            $(this).toggleClass("active");
+            $(this).next(".admin-side-bar-menu-item-child").toggleClass("active");
+        });
+    };
+
+    function init() {
+        handleToggleMultipleMenu();
+    }
+
+    init();
+});

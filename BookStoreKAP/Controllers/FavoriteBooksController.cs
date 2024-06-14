750dd6a0b7a45e913dfbc60fe45624e473a9915e
kiet pham <kietpyyy@gmail.com>
2024-06-14 15:14:01 +0700

    Update all+ add FavoriteBooks

---
+++ BookStoreKAP/Controllers/FavoriteBooksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreKAP.Controllers
+{
+    public class FavoriteBooksController : Controller
+    {
+        public IActionResult Index()
+
+        {
+            return View();
+        }
+    }
+}

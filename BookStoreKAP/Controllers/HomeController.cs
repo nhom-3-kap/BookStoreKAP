7c3aaf5420e882497d9f43dee624fc4356d04246
Tài Lê <taic21a.th1@gmail.com>
2024-06-26 03:02:28 +0700

    Update: Cart

--- BookStoreKAP/Controllers/HomeController.cs
+++ BookStoreKAP/Controllers/HomeController.cs
@@ -10,51 +10,50 @@ using System.Diagnostics;
 
 namespace BookStoreKAP.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
-		private readonly BookStoreKAPDBContext _context;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly BookStoreKAPDBContext _context;
 
-		public HomeController(ILogger<HomeController> logger, BookStoreKAPDBContext context)
-		{
-			_logger = logger;
-			_context = context;
-		}
+        public HomeController(ILogger<HomeController> logger, BookStoreKAPDBContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
         public IActionResult Index()
         {
-            var qrCode = new QrCodeService(BankName.MBBank, "0902905361");
-            var a = qrCode.BuildQRString(1000, "Test QR Code");
+
             var tags = _context.Tags.Include(x => x.Books).ToList();
             return View(tags);
         }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error(int statusCode)
-		{
-			if (statusCode == HttpStatusCodeConstant.NOT_FOUND)
-			{
-				return View("NotFound");
-			}
-			else if (statusCode == HttpStatusCodeConstant.INTERNAL_ERROR)
-			{
-				return View("InternalError");
-			}
-			else
-			{
-				return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-			}
-		}
-		[HttpPost]
-		public IActionResult HandleSearch(SearchDTO req)
-		{
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error(int statusCode)
+        {
+            if (statusCode == HttpStatusCodeConstant.NOT_FOUND)
+            {
+                return View("NotFound");
+            }
+            else if (statusCode == HttpStatusCodeConstant.INTERNAL_ERROR)
+            {
+                return View("InternalError");
+            }
+            else
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
+        [HttpPost]
+        public IActionResult HandleSearch(SearchDTO req)
+        {
 
-			return Redirect($"{RouteConstant.LIST}?input={req.KeySearch}&Hambuger=Search");
-		}
-	}
+            return Redirect($"{RouteConstant.LIST}?input={req.KeySearch}&Hambuger=Search");
+        }
+    }
 }
--- BookStoreKAP/Controllers/PayController.cs
+++ BookStoreKAP/Controllers/PayController.cs
@@ -1,63 +1,93 @@
 ﻿using BookStoreKAP.Data;
+using BookStoreKAP.Models;
 using BookStoreKAP.Models.DTO;
 using BookStoreKAP.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Drawing;
+using BookStoreKAP.Services;
+using BookStoreKAP.Common.Constants;
 
 namespace BookStoreKAP.Controllers
 {
-	public class PayController : Controller
-	{
-		private readonly BookStoreKAPDBContext _context;
+    public class PayController : Controller
+    {
+        private readonly BookStoreKAPDBContext _context;
 
-		public PayController(BookStoreKAPDBContext context)
-		{
-			_context = context;
-		}
+        public PayController(BookStoreKAPDBContext context)
+        {
+            _context = context;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
-		[HttpPost]
-		public IActionResult Payment(PaymentDTO req)
-		{
-			var user = _context.Users.FirstOrDefault(x => x.UserName == this.User.Identity.Name);
-			var order = new Order()
-			{
-				Address = req.Address,
-				CustomerID = user.Id,
-				PaymentMethod = req.PaymentMethod,
-				OrderDate = new DateTime(),
+        public IActionResult Index(double totalPrice)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.UserName == this.User.Identity.Name);
+
+            ViewBag.TotalPrice = totalPrice;
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Payment(PaymentDTO req)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.UserName == this.User.Identity.Name);
+            var order = new Order()
+            {
+                Address = req.Address,
+                CustomerID = user.Id,
+                PaymentMethod = req.PaymentMethod,
+                OrderDate = new DateTime(),
 
 
-			};
-			_context.Orders.Add(order);
-			_context.SaveChanges();
-			var cart = _context.Carts.FirstOrDefault(x => x.UserID == user.Id && x.Status == StatusCart.PENDING);
+            };
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+            var cart = _context.Carts.FirstOrDefault(x => x.UserID == user.Id && x.Status == StatusCart.PENDING);
 
-			var cartItems = _context.CartItems.Include(x => x.Book).Where(x => x.CartID == cart.ID).ToList();
+            var cartItems = _context.CartItems.Include(x => x.Book).Where(x => x.CartID == cart.ID).ToList();
 
-			var orderDetails = new List<OrderDetail>();
-			var total = 0.0;
-			cartItems.ForEach(cartItem =>
-			{
-				total += cartItem.Price * cartItem.Quantity;
-				orderDetails.Add(new OrderDetail
-				{
-					BookID = cartItem.BookID,
-					OrderID = order.ID,
-					Price = cartItem.Price,
-					Quantity = cartItem.Quantity,
-				});
-			});
-			order.Total = total;
-			cart.Status = StatusCart.DONE;
-			_context.Carts.Update(cart);
-			_context.Orders.Update(order);
-			_context.OrderDetails.AddRange(orderDetails);
-			_context.SaveChanges();
-			return View();
-		}
-	}
+            var orderDetails = new List<OrderDetail>();
+            var total = 0.0;
+            cartItems.ForEach(cartItem =>
+            {
+                total += cartItem.Price * cartItem.Quantity;
+                orderDetails.Add(new OrderDetail
+                {
+                    BookID = cartItem.BookID,
+                    OrderID = order.ID,
+                    Price = cartItem.Price,
+                    Quantity = cartItem.Quantity,
+                });
+            });
+            order.Total = total;
+            cart.Status = StatusCart.DONE;
+            _context.Carts.Update(cart);
+            _context.Orders.Update(order);
+            _context.OrderDetails.AddRange(orderDetails);
+            _context.SaveChanges();
+            TempData[ToastrConstant.SUCCESS_MSG] = "Payment success";
+            return Redirect($"{RouteConstant.HOME}");
+        }
+
+        public IActionResult CreateQRPaymentAPI(double totalPrice)
+        {
+            // Khởi tạo dịch vụ QRCodeService với thông tin cần thiết
+            var qrCodeService = new QrCodeService(BankName.MBBank, "0902905361");
+            var qrString = qrCodeService.BuildQRString(totalPrice, "Send to KAPBookStore");
+
+            // Tạo đối tượng QRCodeGenerator
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(qrString, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCoder.Base64QRCode(qrCodeData);
+
+            var base64QR = qrCode.GetGraphic(5);
+            // Trả về base64 string
+            return Ok(new ResponseAPI<string>() { Success = true, Message = "Generator QR Code Successfully", Data = base64QR });
+        }
+    }
 }
